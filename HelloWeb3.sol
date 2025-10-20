d8a09941a2f780e05e5348a0bf9883e746fe02ed
0x73 <arronf2e@163.com>
2025-10-20 21:48:20 +0800

    Update HelloWeb3.sol

--- HelloWeb3.sol
+++ HelloWeb3.sol
@@ -1,6 +1,10 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT  
+// 软件许可
+
+// Solidity 版本
 pragma solidity ^0.8.21;
 
+// 合约
 contract HelloWeb3 {
-    string public _string = "hello web3";
+    string public _string = "Hello Web3!";
 }
